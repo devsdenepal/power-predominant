4546448eff683755673d6643b07ce06e4fc1f8cc
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-02-25 23:50:20 +0545

    Add files via upload

---
+++ power-predom.py
@@ -0,0 +1,8 @@
+import os
+import sys
+print("POWER-PREDOM")
+print("------------")
+print("(c) Dev. Gautam Kumar")
+statement = "python3 build.py "+ sys.argv[1] +";iexpress /N power-predom.SED"
+os.system(statement)
+
